bdf23f0e95f753f9f6357c1a4c63dc25a6779524
juli1994 <julihartawan06@gmail.com>
2022-08-17 21:09:52 +0700

    update nama email

---
+++ app/src/main/java/com/example/tryapp/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.tryapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+    Handler handler = new Handler();
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+}
